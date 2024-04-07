f2aff4be25147d81eed9a615091312f57b995710
shintaro <shintaro.nakai7140@gmail.com>
2024-04-07 15:32:06 +0900

    modify files to build electron

---
+++ frontend/public/electron.mjs
@@ -0,0 +1,55 @@
+import {app, BrowserWindow,Menu} from 'electron';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import isDev from 'electron-is-dev';
+import { spawn } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let api_path = process.argv[2];
+let children = spawn('demo.exe',{cwd:api_path,shell: true});
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreperences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadURL(
+        isDev
+        ? "http://localhost:3000"
+        : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+    
+}
+
+const template = Menu.buildFromTemplate([{
+    label: '表示',
+    submenu: [
+        {role: 'reload', label: '再読み込み'}
+    ]
+}]);
+
+Menu.setApplicationMenu(template);
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+        console.log('finish app');
+        spawn('taskkill',['/pid',children.pid,'/t','/f'],{shell: true});
+    }
+})
